8364cab82ae216b10cbc47a544d6e687dbc2020d
Kirill <piksarus@gmail.com>
2021-02-17 00:36:47 +0300

    3r commit

--- src/components/StorebBooks/StoreBooks.js
+++ src/components/StorebBooks/StoreBooks.js
@@ -5,24 +5,42 @@ function StoreBooks({}) {
 
     const booksData = [
         {
-
             title: "Java Script и Jquery",
             author: "Дэвид Сойер Макферланд",
             year: 2016,
+            image: "https://eloquentjavascript.net/img/cover.jpg"
+        },
+        {
+            title: "ES6 и не только",
+            author: "Кайл Симпсон",
+            year: 2017,
+            image: "https://eloquentjavascript.net/img/cover.jpg"
+        },
+        {
+            title: "Секреты JavaScript ниндзя",
+            author: "Джон Резиг",
+            year: 2017,
             image: "https://eloquentjavascript.net/img/cover.jpg"
         },
     ]
 
     return (
         <div>
-            <div className={s.container}>
-                {booksData.map(({title}) => (
-                    <h1
-                        key={`${title}`}
-                        data-name={title}
-                    >{title}</h1>
-                ))}
-            </div>
+            {booksData.map(({title, author, year,image}) => (
+                <div className={s.containerItem}>
+                    <img src={image}
+                         className={s.img}
+                         alt="bookImage"/>
+                    <h1>{title}</h1>
+                    <p>{author}</p>
+                    <p>{year}</p>
+
+                    <div>
+                        <button>Редактировать</button>
+                        <button>Удалить</button>
+                    </div>
+                </div>
+            ))}
         </div>
     );
 }
